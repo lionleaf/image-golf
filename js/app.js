55b81eef28b74fe3751b31cb0b647c9c2fbc8797
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2015-09-19 20:04:19 -0400

    Add basic Jade support

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set("views", "./views");
+app.set("view engine", "jade");
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+      res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 var server = app.listen(3000, function () {
